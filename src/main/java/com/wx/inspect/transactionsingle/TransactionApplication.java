ea5aa0a8cabeb8e7a39639aba8591813e98f16d1
wangxiao <wangxiao@ybm100.com>
2019-08-20 20:10:54 +0800

    王啸-项目初始化-2019年08月20日20:10:44

---
+++ src/main/java/com/wx/inspect/transactionsingle/TransactionApplication.java
@@ -0,0 +1,13 @@
+package com.wx.inspect.transactionsingle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionApplication.class, args);
+    }
+
+}
